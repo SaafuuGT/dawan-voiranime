d56b035f5d695f2f71d3ae26f0e6f6e0ef0ace28
SAPH <benmouloud.mehdi13014@gmail.com>
2024-06-24 15:34:25 +0200

    Amélioration page profil

--- asset/js/mon-profil.js
+++ asset/js/mon-profil.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const profileContainer = document.getElementById('profile-container');
+    const profileCard = document.getElementById('profile-card');
 
     // Récupérer les données utilisateur du localStorage
     const storedUser = localStorage.getItem('user');
@@ -10,7 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
     if (storedUser && connected) {
         const user = JSON.parse(storedUser);
     
-
         // Afficher les données utilisateur
         const profilePicture = document.getElementById('profile-picture');
         const profileUsername = document.getElementById('profile-username');
@@ -19,7 +19,17 @@ document.addEventListener('DOMContentLoaded', () => {
         profilePicture.innerHTML = `<img src="${user.img}" alt="Profile Picture" style="width: 150px; height: 150px; border-radius: 50%;">`;
         profileUsername.innerHTML = `<p><strong>Username:</strong> ${user.username}</p>`;
         profileEmail.innerHTML = `<p><strong>Email:</strong> ${user.email}</p>`;
+
+        profileCard.style.display = 'block';
     } else {
-        profileContainer.innerHTML = '<p>Aucun utilisateur trouvé. Veuillez vous connecter ou vous inscrire.</p>';
+        profileCard.style.display = 'none';
+        
+        // Créer dynamiquement le message "Aucun utilisateur trouvé"
+        const noUserMessage = document.createElement('div');
+        noUserMessage.classList.add('container', 'mt-5', 'd-flex', 'justify-content-center');
+        noUserMessage.innerHTML = '<p>Aucun utilisateur trouvé. Veuillez vous connecter ou vous inscrire.</p>';
+
+        // Ajouter le message au profil container
+        profileContainer.appendChild(noUserMessage);
     }
 });
